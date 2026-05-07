3da4e229ce172547f38627b179d60d39be30e7f5
wavyfy <teamwavyfy@gmail.com>
2026-05-07 18:20:50 +0530

    feat: add vercel configuration and enable unsplash image optimization

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-      
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
